ec30903d19edf1eef038277e3c3b350e354ac503
ediardor.contato@outlook.com <eduardor.contato@outlook.com>
2021-05-07 00:56:18 -0300

    Finished component Highlight + tests

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,10 +2,12 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
   subtitle: 'Heading 2',
+  backgroundImage: 'img/rede-dead-img.jpg',
   buttonLabel: 'Buy now',
   buttonLink: '/rdr2'
 }
@@ -25,5 +27,50 @@ describe('<Highlight />', () => {
     expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
 
     // expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
   })
 })
