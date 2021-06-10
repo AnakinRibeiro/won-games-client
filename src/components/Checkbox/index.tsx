9ae93c9f0897fae55f3a6432e13659d19e91a88e
ediardor.contato@outlook.com <eduardor.contato@outlook.com>
2021-06-10 02:14:32 -0300

    Finishing components Checkbox and Radio (onChange method on a controlled component, useState, isChecked...)

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,54 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
+  onCheck?: (status: boolean) => void
+  isChecked?: boolean
   label?: string
   labelFor?: string
   labelColor?: 'white' | 'black'
-}
+  value?: string | ReadonlyArray<string> | number
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
+  onCheck,
+  // checkbox sempre é renderizado sem estar checked
+  isChecked = false,
   label,
   labelFor = '',
-  labelColor = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+  labelColor = 'white',
+  value,
+  ...props
+}: CheckboxProps) => {
+  // controlled components (state)
+  const [checked, setChecked] = useState(isChecked)
+
+  const onChange = () => {
+    const status = !checked // true => false => true
+    setChecked(status)
+
+    // verificação binária
+    !!onCheck && onCheck(status)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+        value={value}
+        // permite que o usuário passe qualquer outra prop presente no HTMLInputElement
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Checkbox
